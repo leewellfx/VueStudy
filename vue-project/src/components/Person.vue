fe69489b76e9346a5a44e65ee52a624b41b64466
Leewellwaste <zzzz_justice@qq.com>
2024-05-25 18:01:36 +0800

    1

--- vue-project/src/components/Person.vue
+++ vue-project/src/components/Person.vue
@@ -2,7 +2,7 @@
  * @Author: Leewellwaste zzzz_justice@qq.com
  * @Date: 2024-05-24 16:19:13
  * @LastEditors: Leewellwaste zzzz_justice@qq.com
- * @LastEditTime: 2024-05-25 16:04:53
+ * @LastEditTime: 2024-05-25 18:00:34
  * @FilePath: \VueStudy\vue-project\src\components\Person.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -44,7 +44,6 @@ import {ref} from 'vue'
 // 数据+reactive() 可以转化为响应示
 let address =ref('北京')
 let car = reactive({brand:'Benz',prie:100})
-
 
 
 // 方法
