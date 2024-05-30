9ae82308f4de061f16c1b20bf16c14241f7d8437
Leewellwaste <zzzz_justice@qq.com>
2024-05-30 17:37:21 +0800

    530

--- vue-project/src/components/Person.vue
+++ vue-project/src/components/Person.vue
@@ -2,7 +2,7 @@
  * @Author: Leewellwaste zzzz_justice@qq.com
  * @Date: 2024-05-24 16:19:13
  * @LastEditors: Leewellwaste zzzz_justice@qq.com
- * @LastEditTime: 2024-05-27 17:23:36
+ * @LastEditTime: 2024-05-30 17:35:17
  * @FilePath: \VueStudy\vue-project\src\components\Person.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -24,30 +24,15 @@
       <br>
       车子型号:{{car.brand}}
       <br>
-      车子价格:{{ car.prie}}W
+      车子价格:{{ car.price}}W
       <br>
       <button @click="changprice" > 修改价格</button>
       <button @click="changaddress"> 更换地点</button>
+      <button @click="changband">修改品牌</button>
+      <button @click="changcar"> 更换车子</button>
       <br>
       <br>
-      游戏信息:
-      <ui class="personul">
-        <li v-for=" game in games" :key="game.id">{{ game.gamename }}</li>
-      </ui>
-      <br>
-      <button @click="changGame1">修改第一个游戏名字</button>
-      <br>
-      <br>
-
-      
-    测试:
-    {{ test.a.b.c }}
-    <button @click="changValue"> Tap</button>
     </div>
-
-
-
-
   </template>
    
 
@@ -59,42 +44,27 @@ import {ref,reactive} from 'vue'
 
 // 数据+reactive() 可以转化为响应示
 let address =ref('北京')
-let car = ref({brand:'Benz',prie:100})
-
-let games =ref([
-  {id:'game1',gamename:'元神'},
-  {id:'game2',gamename:'王者'},
-  {id:'game3',gamename:'吃鸡'},
-])
-
-let test = reactive( {
-  a:{
-    b:{
-      c:666
-    }
-
-  }
+let car = ref({
+  brand:'Benz',
+  price:100
 })
+
+
 // 方法
 function changprice(){
-  car.value.prie += 10
+  car.value.price += 10
 }
 
 function changaddress(){
   address.value = '上海'
+} 
+
+function changband(){
+  car.value.brand = '奔驰'
 }
 
-function changGame1(){
-  // games[0].gamename = 'LOL'
-
-  games.value[0].gamename = '魔兽'
-
-
-  
-}
-
-function changValue(){
-  test.a.b.c += 1
+function changcar(){
+  Object.assign(car.value,{brand:'宝马',price:40});
 }
 </script>
 
